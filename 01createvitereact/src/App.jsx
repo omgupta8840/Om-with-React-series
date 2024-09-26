e756782984c629b1456c17cab3491af874a4d09a
omgupta8840 <jayg8840@gmail.com>
2024-09-27 00:40:48 +0530

    previous react project

---
+++ 01createvitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from "./chai"
+
+
+function App() {
+
+
+  return (
+    <>
+  <Chai />
+  <h1>hello om gupta</h1>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus enim fug</p>
+  </>
+  )
+}
+
+export default App
